cddb59cf6e672aae16a9eeedf3f52f77b7197563
Joshua Hume <valencelectron@hotmail.com>
2021-09-05 21:14:13 +0900

    css to scss

--- app/javascript/packs/Components/Maps/MapboxMap.tsx
+++ app/javascript/packs/Components/Maps/MapboxMap.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import './MapboxMap.css';
 import mapboxgl, { LngLatLike } from 'mapbox-gl';
 
 const MapboxMap = () => {
